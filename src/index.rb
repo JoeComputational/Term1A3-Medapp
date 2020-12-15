bcee1e4ea321486cf7dfc57fdc44b266c4a42275
JoeComputational <userade11@gmail.com>
2020-12-15 11:31:57 +1000

    Added conversion methods and value/input check to initiate required conversion

--- src/index.rb
+++ src/index.rb
@@ -97,3 +97,74 @@ def save_data(curr_data)
       item.print_medication
     end
   end
+
+  def get_value(message)
+    value = 0
+    while true
+      puts message
+      value = gets.chomp.to_f
+      if value < 0
+        puts "Please enter a valid value"
+      else
+        break
+      end
+    end
+    value
+  end
+  
+  def mass_conversion(prompt)
+      mass_choices = ["KG to G", "G to mG", "mG to uG", "uG to mG", "mG to G",
+                      "G to KG", "Back to Main Menu", "Exit Application"]
+      while true
+        mass_choice = prompt.select("Please Select: ",mass_choices)
+        choice = mass_choices.find_index(mass_choice)+1
+        if choice >=1 && choice <= 6
+          value = get_value("Please enter value to convert: ")
+          if choice == 1
+            puts  value.to_s + " Kg = " + (value*1000).to_s + " G"
+          elsif  choice == 2
+            puts  value.to_s + " G = " + (value*1000).to_s + " mG"
+          elsif  choice == 3
+            puts  value.to_s + " mg = " + (value*1000).to_s + " uG"
+          elsif  choice == 4
+            puts  value.to_s + " uG = " + (value/1000).to_s + " mG"
+          elsif  choice == 5
+            puts  value.to_s + " mG = " + (value/1000).to_s + " G"
+          elsif  choice == 6
+            puts  value.to_s + " G = " + (value/1000).to_s + " KG"
+          end
+        else
+          break
+        end
+      end
+      choice
+  end
+  
+  def volume_conversion(prompt)
+  
+      volume_choices = ["KL to L", "L to mL", "mL to uL", "uL to mL", "mL to L",
+                      "L to KL", "Back to Main Menu", "Exit Application"]
+      while true
+        volume_choice = prompt.select("Please Select: ",volume_choices)
+        choice = volume_choices.find_index(volume_choice)+1
+        if choice >=1 && choice <= 6
+          value = get_value("Please enter value to convert: ")
+          if choice == 1
+            puts  value.to_s + " KL = " + (value*1000).to_s + " L"
+          elsif  choice == 2
+            puts  value.to_s + " L = " + (value*1000).to_s + " mL"
+          elsif  choice == 3
+            puts  value.to_s + " mg = " + (value*1000).to_s + " uL"
+          elsif  choice == 4
+            puts  value.to_s + " uL = " + (value/1000).to_s + " mL"
+          elsif  choice == 5
+            puts  value.to_s + " mL = " + (value/1000).to_s + " L"
+          elsif  choice == 6
+            puts  value.to_s + " L = " + (value/1000).to_s + " KL"
+          end
+        else
+          break
+        end
+      end
+      choice
+  end
