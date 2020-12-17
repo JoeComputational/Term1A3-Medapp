450aa49bbef311577e166e00efeeb1c68c1ab75f
JoeComputational <userade11@gmail.com>
2020-12-17 20:56:00 +1000

    Added Supportive Supplements and Minerals - now has lots of info

--- src/index.rb
+++ src/index.rb
@@ -93,6 +93,19 @@ def read_medication(read_data)
   read_data
 end
 
+def vitamin_use(prompt)
+  vitamins = ['Vitamin D- Helps your body absorb calcium', 'Vitamin C- Immune system Boost/growth', 'Vitamin B Complex- Increase Energy, Brain Function and Cell Metabolism', 'IVP– Intravenous Push',
+                 'Iron- Increase Energy, Brain Function and Promotes Healthy Red Blood Cells', 'Zinc- Immune Support/Energy Absorbtion', 'Calcium- Promotes Strong Bones and Teeth', 
+                 'Magnesium- Bone Health/Energy Production (also good for cramps)', 'Folate/Folic Acid- Aids Fetus Development, Fights Depression and combats Inflammation']
+  puts 'List of Useful supplements'
+  vitamins.each do |item|
+    puts item
+  end
+  menu_choices = ['Back to Main Menu', 'Exit Application']
+  menu_choice = prompt.select('Please Select: ', menu_choices)
+  menu_choices.find_index(menu_choice) + 1
+end
+
 def display_definitions(prompt)
   definitions = ['IM– Intramuscular', 'IO– Intraosseous', 'IV– Intravenous', 'IVP– Intravenous Push',
                  'ID – Intradermal', 'IN – Intranasal', 'IP – Intraperitoneal', 'IT – Intrathecal', 'IVPB – Intravenous piggyback',
@@ -283,22 +296,25 @@ def main_loop
   read_data = Array.new
   while true
     puts ""
-    menu_choices = ['Medication Lists and Information', 'Medication Route Definitions', 'Volume Unit Conversion', 'Mass Unit Conversion', 'BMI Calculater', 'Exit']
+    menu_choices = ['Medication Lists and Information', 'Supplements and Minerals', 'Medication Route Definitions', 'Volume Unit Conversion', 'Mass Unit Conversion', 'BMI Calculater', 'Exit']
     menu_choice = prompt.select('Please Select From the Following Options: ', menu_choices)
     choice = menu_choices.find_index(menu_choice) + 1
     if choice == 1
       value = medication_menu(curr_data, prompt, read_data)
       break if value == 5 || value == 3
     elsif choice == 2
-      value = display_definitions(prompt)
+      value = vitamin_use(prompt)
       break if value == 8
     elsif choice == 3
-      value = volume_conversion(prompt)
+      value = display_definitions(prompt)
       break if value == 8
     elsif choice == 4
+      value = volume_conversion(prompt)
+      break if value == 8
+    elsif choice == 5
       value = mass_conversion(prompt)
       break if value == 3
-    elsif choice == 5
+    elsif choice == 6
       value = bmi_calculator(prompt)
       break if value == 3
     else
