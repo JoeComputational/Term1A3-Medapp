bdbc5c3e128c8a102ba66d54a5c6fd6c1588374d
JoeComputational <userade11@gmail.com>
2020-12-19 10:52:39 +1000

    Fixed choice que in def and sups, working correctly - time to debug

--- src/methods/defandsup.rb
+++ src/methods/defandsup.rb
@@ -1,29 +1,29 @@
 def vitamin_use(prompt)
-    vitamins = ['Vitamin D- Helps your body absorb calcium', 'Vitamin C- Immune system Boost/growth', 'Vitamin B Complex- Increase Energy, Brain Function and Cell Metabolism', 'IVP– Intravenous Push',
-                   'Iron- Increase Energy, Brain Function and Promotes Healthy Red Blood Cells', 'Zinc- Immune Support/Energy Absorbtion', 'Calcium- Promotes Strong Bones and Teeth', 
-                   'Magnesium- Bone Health/Energy Production (also good for cramps)', 'Folate/Folic Acid- Aids Fetus Development, Fights Depression and combats Inflammation']
-    puts 'List of Useful supplements'.colorize(:blue)
-    vitamins.each do |item|
-      puts item
-    end
-    menu_choices = ['Back to Main Menu', 'Exit Application']
-    menu_choice = prompt.select('Please Select: ', menu_choices)
-    menu_choices.find_index(menu_choice) + 1
-    puts "\e[H\e[2J"
-    loading_screen("T1A3 Ruby Code Terminal APP ".colorize(:blue))
+  vitamins = ['Vitamin D- Helps your body absorb calcium', 'Vitamin C- Immune system Boost/growth', 'Vitamin B Complex- Increase Energy, Brain Function and Cell Metabolism', 'IVP– Intravenous Push',
+              'Iron- Increase Energy, Brain Function and Promotes Healthy Red Blood Cells', 'Zinc- Immune Support/Energy Absorbtion', 'Calcium- Promotes Strong Bones and Teeth',
+              'Magnesium- Bone Health/Energy Production (also good for cramps)', 'Folate/Folic Acid- Aids Fetus Development, Fights Depression and combats Inflammation']
+  puts 'List of Useful supplements'.colorize(:blue)
+  vitamins.each do |item|
+    puts item
   end
-  
-  def display_definitions(prompt)
-    definitions = ['IM–  Intramuscular', 'IO – Intraosseous', 'IV – Intravenous', 'IVP – Intravenous Push',
-                   'ID – Intradermal', 'IN – Intranasal', 'IP – Intraperitoneal', 'IT – Intrathecal', 'IVPB – Intravenous piggyback',
-                   'p.o – By mouth', 'SC / SubQ – Subcutaneous', 'SL – Sublingual', 'top. – Topical', 'vag. – Vaginally']
-    puts 'List of Definitions'.colorize(:blue)
-    definitions.each do |item|
-      puts item
-    end
-    menu_choices = ['Back to Main Menu', 'Exit Application']
-    menu_choice = prompt.select('Please Select: ', menu_choices)
-    menu_choices.find_index(menu_choice) + 1
-    puts "\e[H\e[2J"
-    loading_screen("T1A3 Ruby Code Terminal APP ".colorize(:blue))
+  menu_choices = ['Back to Main Menu', 'Exit Application']
+  menu_choice = prompt.select('Please Select: ', menu_choices)
+  puts "\e[H\e[2J"
+  loading_screen("T1A3 Ruby Code Terminal APP ".colorize(:blue))
+  menu_choices.find_index(menu_choice) + 1
+end
+
+def display_definitions(prompt)
+  definitions = ['IM–  Intramuscular', 'IO – Intraosseous', 'IV – Intravenous', 'IVP – Intravenous Push',
+                 'ID – Intradermal', 'IN – Intranasal', 'IP – Intraperitoneal', 'IT – Intrathecal', 'IVPB – Intravenous piggyback',
+                 'p.o – By mouth', 'SC / SubQ – Subcutaneous', 'SL – Sublingual', 'top. – Topical', 'vag. – Vaginally']
+  puts 'List of Definitions'.colorize(:blue)
+  definitions.each do |item|
+    puts item
   end
+  menu_choices = ['Back to Main Menu', 'Exit Application']
+  menu_choice = prompt.select('Please Select: ', menu_choices)
+  puts "\e[H\e[2J"
+  loading_screen("T1A3 Ruby Code Terminal APP ".colorize(:blue))
+  menu_choices.find_index(menu_choice) + 1
+end
